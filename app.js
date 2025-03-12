e56cea34350931e03e0a9571dd216345c3958426
jjdavenport <jordanjamesdavenport@gmail.com>
2025-03-12 22:15:05 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+const PORT = process.env.PORT || 3000;
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on http://${HOSTNAME}:${PORT}`);
+});
